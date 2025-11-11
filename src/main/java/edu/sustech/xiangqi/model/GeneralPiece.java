976d4804406ab473ff4b43714217139e0f344cd8
sharknnss <18162564732@163.com>
2025-11-11 16:03:49 +0800

    General

--- src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
+++ src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
@@ -1,17 +1,33 @@
 package edu.sustech.xiangqi.model;
 
-/**
- * 帅/将
- */
 public class GeneralPiece extends AbstractPiece {
 
     public GeneralPiece(String name, int row, int col, boolean isRed) {
         super(name, row, col, isRed);
     }
 
+
     @Override
     public boolean canMoveTo(int targetRow, int targetCol, ChessBoardModel model) {
-        // TODO: 实现将/帅的移动规则
-        return true;
+        int currentRow = getRow();
+        int currentCol = getCol();
+
+        if (currentRow == targetRow && currentCol == targetCol) {
+            return false;
+        }
+
+        int rowDiff = targetRow - currentRow;
+        int colDiff = Math.abs(targetCol - currentCol);
+
+        // 兵/卒的移动规则：
+        // 1.九宫内走一格直线；
+        // 2.不得对面见将
+
+        if (isRed()) {
+            return targetRow >= 0 && targetRow <= 10 && Math.abs(rowDiff) == 1 && colDiff == 0;
+        }
+        else {
+            return targetRow >= 0 && targetRow <= 10 && Math.abs(rowDiff) == 1 && colDiff == 0;
+        }
     }
 }
