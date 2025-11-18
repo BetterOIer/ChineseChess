3aab23df68180f0ede63cc91f2c415e1f4328e61
Gracekite <2155173747@qq.com>
2025-11-18 12:26:11 +0800

    User Interface reconstruct Part 1

---
+++ src/main/java/edu/sustech/xiangqi/ui/ArchiveManager.java
@@ -0,0 +1,12 @@
+package edu.sustech.xiangqi.ui;
+
+
+import edu.sustech.xiangqi.model.ChessBoardModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ArchiveManager extends JPanel{
+
+}
--- src/main/java/edu/sustech/xiangqi/ui/WelcomePage.java
+++ src/main/java/edu/sustech/xiangqi/ui/WelcomePage.java
@@ -1,32 +1,25 @@
 package edu.sustech.xiangqi.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 import edu.sustech.xiangqi.model.ChessBoardModel;
 
 public class WelcomePage {
 
     private JFrame frameWelcome;
-    private JButton loginButton;
-    private JTextField userName, password;
+    private JButton boardOnlyButton;
+    private JButton remoteGameButton;
+    private JButton withAIButton;
+    /* private JTextField userName, password; */
 
-    private void switchToBoards(){
-        String userNameText = userName.getText();
-        String passwordText = password.getText();
-        if((!userNameText.equals(""))&& (!passwordText.equals(""))){
+    private void switchToLoginPage(){
             frameWelcome.setVisible(false);
 
             JFrame frame = new JFrame("中国象棋");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JLabel label = new JLabel("开始");
-            label.setLocation(600, 100);
-            label.setSize(100, 50);
-            frame.add(label);
-            JButton button = new JButton("start");
-            button.setLocation(600, 200);
-            button.setSize(100, 50);
-            frame.add(button);
 
             ChessBoardModel model = new ChessBoardModel();
             ChessBoardPanel boardPanel = new ChessBoardPanel(model);
@@ -36,21 +29,120 @@ public class WelcomePage {
             frame.pack();
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
-        }
+    }
+
+    private void switchToArchMgr(){
+        JFrame frame = new JFrame("中国象棋");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ArchiveManager boardPanel = new ArchiveManager();
+        frame.add(boardPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frameWelcome.setVisible(false);
+        frame.setVisible(true);
     }
 
     public WelcomePage(){
-        frameWelcome  = new JFrame("Welcome!");
+        frameWelcome  = new JFrame("中国象棋");
         frameWelcome.setLayout(null);
-        frameWelcome.setSize(300,300);
+        frameWelcome.setSize(768,768);
+        frameWelcome.setLocationRelativeTo(null);
         frameWelcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameWelcome.getContentPane().setBackground(new Color(220, 179, 92));
+        // 设置窗口可缩放的最小尺寸并在缩放时强制维持该最小值
+        frameWelcome.setMinimumSize(new Dimension(600, 768));
+        /* frameWelcome.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                int minW = 600, minH = 600;
+                int w = Math.max(frameWelcome.getWidth(), minW);
+                int h = Math.max(frameWelcome.getHeight(), minH);
+                if (w != frameWelcome.getWidth() || h != frameWelcome.getHeight()) {
+                    frameWelcome.setSize(w, h);
+                }
+            }
+        }); */
 
         JLabel msgGreetings = new JLabel("欢迎来玩中国象棋！");
-        msgGreetings.setLocation(10, 10);
-        msgGreetings.setSize(120,40);
+        msgGreetings.setFont(new Font("SimHei", Font.BOLD, 36));
+        int greetMsgLabelWidth = 400;
+        int greetMsgLabelHeight = 60;
+        int greetMsgLabelOriginX = (frameWelcome.getWidth()-greetMsgLabelWidth)/2;
+        int greetMsgLabelOriginY = (frameWelcome.getHeight()-greetMsgLabelHeight)/2/4;
+        msgGreetings.setHorizontalAlignment(SwingConstants.CENTER);
+        msgGreetings.setVerticalAlignment(SwingConstants.CENTER);
+        msgGreetings.setBounds(greetMsgLabelOriginX, greetMsgLabelOriginY, greetMsgLabelWidth, greetMsgLabelHeight);
+        frameWelcome.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                int x = (frameWelcome.getWidth()-msgGreetings.getWidth())/2;
+                msgGreetings.setLocation(Math.max(0, x), msgGreetings.getY());
+            }
+        });
         frameWelcome.add(msgGreetings);
 
-        JLabel userNameTip = new JLabel("用户名：");
+        boardOnlyButton = new JButton("仅棋盘");
+        boardOnlyButton.setFont(new Font("SimHei", Font.BOLD, 20));
+        int boardOnlyButtonWidth = 400;
+        int boardOnlyButtonHeight = 60;
+        int boardOnlyButtonOriginX = (frameWelcome.getWidth()-boardOnlyButtonWidth)/2;
+        int boardOnlyButtonOriginY = (frameWelcome.getHeight()-boardOnlyButtonHeight)/2;
+        boardOnlyButton.setBounds(boardOnlyButtonOriginX, boardOnlyButtonOriginY, boardOnlyButtonWidth, boardOnlyButtonHeight);
+        frameWelcome.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+            int boardOnlyButtonNowX = (frameWelcome.getWidth() - boardOnlyButton.getWidth()) / 2;
+            boardOnlyButton.setLocation(Math.max(0, boardOnlyButtonNowX), boardOnlyButton.getY());
+            }
+        });
+        frameWelcome.add(boardOnlyButton);
+        boardOnlyButton.addActionListener(e1->{
+            //TODO: this is a temp function. 
+            switchToLoginPage();
+        });
+
+        remoteGameButton = new JButton("联机游戏");
+        remoteGameButton.setFont(new Font("SimHei", Font.BOLD, 20));
+        int remoteGameButtonWidth = 400;
+        int remoteGameButtonHeight = 60;
+        int remoteGameButtonOriginX = (frameWelcome.getWidth()-remoteGameButtonWidth)/2;
+        int remoteGameButtonOriginY = boardOnlyButton.getY() + boardOnlyButton.getHeight() + 40;
+        remoteGameButton.setBounds(remoteGameButtonOriginX, remoteGameButtonOriginY, remoteGameButtonWidth, remoteGameButtonHeight);
+        // 保证按钮在窗口水平居中：监听窗口大小变化而不是按钮本身
+        frameWelcome.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+            int remoteGameButtonNowX = (frameWelcome.getWidth() - remoteGameButton.getWidth()) / 2;
+            remoteGameButton.setLocation(Math.max(0, remoteGameButtonNowX), remoteGameButton.getY());
+            }
+        });
+        frameWelcome.add(remoteGameButton);
+        remoteGameButton.addActionListener(e1->{
+            switchToLoginPage();
+        });
+
+        withAIButton = new JButton("人机");
+        withAIButton.setFont(new Font("SimHei", Font.BOLD, 20));
+        int withAIButtonWidth = 400;
+        int withAIButtonHeight = 60;
+        int withAIButtonOriginX = (frameWelcome.getWidth()-withAIButtonWidth)/2;
+        int withAIButtonOriginY = remoteGameButton.getY() + remoteGameButton.getHeight() + 40;
+        withAIButton.setBounds(withAIButtonOriginX, withAIButtonOriginY, withAIButtonWidth, withAIButtonHeight);
+        // 保证按钮在窗口水平居中：监听窗口大小变化而不是按钮本身
+        frameWelcome.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+            int withAIButtonNowX = (frameWelcome.getWidth() - withAIButton.getWidth()) / 2;
+            withAIButton.setLocation(Math.max(0, withAIButtonNowX), withAIButton.getY());
+            }
+        });
+        frameWelcome.add(withAIButton);
+        /* withAIButton.addActionListener(e1->{
+            switchToLoginPage();
+        }); */
+
+        /* JLabel userNameTip = new JLabel("用户名：");
         userNameTip.setLocation(10, 60);
         userNameTip.setSize(120,40);
         frameWelcome.add(userNameTip);
@@ -70,13 +162,13 @@ public class WelcomePage {
         password.setSize(100, 40);
         frameWelcome.add(password);
 
-        loginButton = new JButton("登录");
-        loginButton.setLocation(10, 160);
-        loginButton.setSize(100, 40);
-        frameWelcome.add(loginButton);
-        loginButton.addActionListener(e1->{
+        boardOnlyButton = new JButton("登录");
+        boardOnlyButton.setLocation(10, 160);
+        boardOnlyButton.setSize(100, 40);
+        frameWelcome.add(boardOnlyButton);
+        boardOnlyButton.addActionListener(e1->{
             switchToBoards();
-        });
+        }); */
     }
 
     public JFrame getWelcomeFrame(){
